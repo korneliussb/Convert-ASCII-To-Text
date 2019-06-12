89271842a0b1fa42508ff94f1ef5fb97b7418499
korneliussb <korneliussb@student.ce.undip.ac.id>
2019-06-13 01:50:07 +0700

    Convert ASCII to Text and Text to ASCII

---
+++ ascii2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+int main(){
+	//clrscr();
+	char a= '2', b='1', c='1', d='2', e='0', f='1', g='1', h='6', i='1', j='2';
+	char k='0', l='0', m='0', n='7';
+	printf("\n Kode ASCII dari karakter 21120116120007 adalah : %d", a);
+	printf(" %d", b); printf(" %d", c); printf(" %d", d); printf(" %d", e);
+	printf(" %d", f); printf(" %d", g); printf(" %d", h); printf(" %d", i);
+	printf(" %d", j); printf(" %d", k); printf(" %d", l); printf(" %d", m);
+	printf(" %d", n);
+
+
+   return 0;
+}
+
+
+//	char greeting[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
+//   printf("Greeting message: %s\n", greeting );
+   
+//getch();
---
+++ asciiToText.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+int main(){
+	//clrscr();
+	int a= 50, b=49, c=49, d=50, e=48, f=49, g=49, h=54, i=49, j=50;
+	int k=48, l=48, m=48, n=55;
+	printf("\n Character dari ASCII berikut:50 49 49 50 48 49 49 54 49 50 48 48 50 50 adalah %c", a);
+	printf(" %c", b); printf(" %c", c); printf(" %c", d); printf(" %c", e);
+	printf(" %c", f); printf(" %c", g); printf(" %c", h); printf(" %c", i);
+	printf(" %c", j); printf(" %c", k); printf(" %c", l); printf(" %c", m);
+	printf(" %c", n);
+
+   return 0;
+}
+
+
+//	char greeting[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
+//   printf("Greeting message: %s\n", greeting );
+   
+//getch();
